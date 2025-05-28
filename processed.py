5926dafaacb5bef593ab7df2b7edc6536a3d7f89
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2025-05-28 15:57:45 +0530

    integrated llm

--- processed.py
+++ processed.py
@@ -1,4 +1,8 @@
 import json
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from llm_helper import llm
 
 def process_json(raw_file_path, processed_file_path ="data/pre_processsed.json") :
     enriched_posts = []
@@ -15,12 +19,30 @@ def process_json(raw_file_path, processed_file_path ="data/pre_processsed.json")
         
         
 def extract_metadata(post):
-    return {
-        'line count': 2,
-        'language': 'English',
-        'tags': ['Internship', 'Career', 'Milestone']
+    template = '''
+    You are given a LinkedIn post. You need to extract number of lines, language of the post and tags.
+    1. Return a valid JSON. No preamble. 
+    2. JSON object should have exactly three keys: line_count, language and tags. 
+    3. tags is an array of text tags. Extract maximum two tags.
+    4. Language should be English or Hinglish (Hinglish means hindi + english)
+    
+    Here is the actual post on which you need to perform this task:  
+    {post}
+    '''
+    
+    pt = PromptTemplate.from_template(template)
+    chain = pt | llm
+    response = chain.invoke(input={'post': post})
+    
+    try:
+        json_parser = JsonOutputParser()
+        res = json_parser.parse(response.content)
+        print("Raw LLM response:", response.content)
+
+    except OutputParserException:
+        raise OutputParserException("Context too big, unable to parse jobs")
+    return res
         
-    }
-          
+ 
 if __name__ == "__main__":
     process_json("data/raw_posts.json", "data/pre_processsed.json")
