14a50c63ce59c4faac1cd943aa0f808e9c4fa770
Shruti Naik <111608883+shrutiiiiin@users.noreply.github.com>
2025-05-29 20:11:47 +0530

    updated file path, added requirement txt

--- fewshot.py
+++ fewshot.py
@@ -3,7 +3,7 @@ import json
 
 
 class FewShotPosts:
-    def __init__(self, file_path="data/processed_posts.json"):
+    def __init__(self, file_path="data/pre_processsed.json"):
         self.df = None
         self.unique_tags = None
         self.load_posts(file_path)
